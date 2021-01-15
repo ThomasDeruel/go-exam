d7e5a8bbfa5f29964d78a92d9a75cf6aa7a745d8
thomas deruel <thomas.deruel@hetic.net>
2021-01-15 15:02:34 +0100

    Partie1: done

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+type Task struct {
+	Description string
+	Done        bool
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("default")
+	return
+}
+func done(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("done")
+	return
+}
+func add(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("add")
+	return
+}
 func main() {
-	fmt.Println("init")
+	//var task []Task
+	
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/done", doneHandler)
+	http.HandleFunc("/add", addHandler)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
